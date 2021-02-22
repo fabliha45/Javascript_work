3b4f8398d60eeb977964d84b68920e0367e4669d
fabliha45 <78662504+fabliha45@users.noreply.github.com>
2021-02-21 23:39:33 -0500

    commit

--- tutorial2_quantities_and_amounts/main.js
+++ tutorial2_quantities_and_amounts/main.js
@@ -12,7 +12,7 @@ const xScale = d3.scaleBand()
  .range([0, window.innerWidth]) //visual variable
 
  //yscale - linear, count
-const yScale = d3.scaleLiner()
+const yScale = d3.scaleLinear()
  .domain([0, d3.max(squirrelData, d => d.count)])
  .range([window.innerHeight, 0])
 
@@ -21,9 +21,14 @@ const svg = d3.select("#barchart")
  .append("svg")
  .attr("width", width)
  .attr("height", height)
- 
+
 
 //bars
+svg.selectAll("rect")
+ .data(squirrelData)
+ .join("rect")
 
-
+//select
+// data join
+//style/draw elements
 })
