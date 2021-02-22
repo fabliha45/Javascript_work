def66c9b5e60fd622e39aebb11ec9f47655113b5
fabliha45 <78662504+fabliha45@users.noreply.github.com>
2021-02-21 23:26:36 -0500

    commit

---
+++ tutorial2_quantities_and_amounts/main.js
@@ -0,0 +1,29 @@
+d3.csv('../data/squirrelActivities.csv', d3.autoType)
+.then(squirrelData => {
+    console.log("data", squirrelData)
+    //constants
+    const width = window.innerWidth *.9;
+    const height = window.innerHeight /3;
+
+//scales
+//xscale = catagorical activities
+const xScale = d3.scaleBand()
+ .domain(squirrelData.map(d=> d.activity))
+ .range([0, window.innerWidth]) //visual variable
+
+ //yscale - linear, count
+const yScale = d3.scaleLiner()
+ .domain([0, d3.max(squirrelData, d => d.count)])
+ .range([window.innerHeight, 0])
+
+ // svg
+const svg = d3.select("#barchart")
+ .append("svg")
+ .attr("width", width)
+ .attr("height", height)
+ 
+
+//bars
+
+
+})
