ffc96ec35d9c906cfd38e1a63235847218ba8865
fabliha45 <78662504+fabliha45@users.noreply.github.com>
2021-03-04 17:09:53 -0500

    commit

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
 const width = window.innerWidth * 0.7,
   height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 },
+  margin = { top: 20, bottom: 60, left: 60, right: 40 },
   radius = 5;
 
 // these variables allow us to access anything we manipulate in init() but need access to in draw().
@@ -17,9 +17,9 @@ let state = {
 };
 
 /* LOAD DATA */
-d3.csv("../data/PimaIndians.csv", d3.autoType).then(raw_data => {
-  console.log("raw_data", raw_data);
-  state.data = raw_data;
+d3.csv("../data/Heart_failure_records.csv", d3.autoType).then(Heart_data => {
+  console.log("data", Heart_data);
+  state.data = Heart_data;
   init();
 });
 
