8ecb543b049155bbc46bafeae079592c9d653fbf
fabliha45 <78662504+fabliha45@users.noreply.github.com>
2022-12-15 17:28:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+        function ouvrirPage() {
+            var a = document.getElementById("search").value;
+
+            if (a === "Italian") {
+                window.open("Campania.html");
+            }
+
+            if (a === "Chinese") {
+                window.open("tatsu.html");
+            }
+
+            if (a === "American") {
+                window.open("Hill Country Barbecue.html");
+            }
+			
+			if (a === "Mexican") {
+                window.open("Caracas Arepa Bar.html");
+            }
+			
+			if (a === "French") {
+                window.open("Café Rue Dix.html");
+            }
+			
+			if (a === "Indian") {
+                window.open("Tandoori Masala.html");
+            }
+			
+			if (a === "Thai") {
+                window.open("Thai Villa.html");
+            }
+			
+			if (a === "Korean") {
+                window.open("Cho Dang Gol.html");
+            }
+			
+			if (a === "Turkish") {
+                window.open("Rocca Cafe & Lounge.html");
+            }
+			
+			if (a === "Seafood") {
+                window.open("Lure Fish Bar.html");
+            }
+
+        }
